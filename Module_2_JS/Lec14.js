6ccc73376cf93c5a5d4b7e1b6129c9791e776576
Your Name <dipeshj2310@gmail.com>
2025-06-29 23:53:18 +0530

    slice and splice add some example

--- Module_2_JS/Lec14.js
+++ Module_2_JS/Lec14.js
@@ -1,1 +1,15 @@
 //! #### Slice and Splice
+
+
+//?Explaination
+// any range value the start point is always inclusive and last point is always exclusive
+// means start from 2 to end at 8 => 2 is include and 8 is not means => 2,3,4,5,6,7
+
+//?Example-1
+var users = ["Ted", "Tim", "Ton", "Sam", "Sor", "Sod"];
+
+console.log(users.slice(1, 3)) //?["Tim","Ton"] // here ending is not inclusive
+
+console.log(users.splice(1, 4)) //?["Tim","Ton","Sam","Sor"] //here inclusive all 
+
+
