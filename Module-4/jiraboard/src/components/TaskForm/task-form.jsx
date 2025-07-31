89de7224aad354cab0f5aab7d5ab9c13289704f0
Your Name <dipeshj2310@gmail.com>
2025-08-01 01:10:25 +0530

    lecture-4 of module-4

--- Module-4/jiraboard/src/components/TaskForm/task-form.jsx
+++ Module-4/jiraboard/src/components/TaskForm/task-form.jsx
@@ -6,20 +6,25 @@ const TaskForm = () => {
             <form>
                 <input type='text' placeholder='Enter Class Details' className='task_input' />
 
-                <div className=''>
-                    <button className='tag'>DEV</button>
-                    <button className='tag'>QA</button>
-                    <button className='tag'>Product Owner</button>
+                <div className='task_form_bottom'>
+
+                    <div>
+                        <button className='tag'>DEV</button>
+                        <button className='tag'>QA</button>
+                        <button className='tag'>Product Owner</button>
+                    </div>
+
+                    <div>
+                        <select className='task_status'>
+                            <option value="Ready For Development">Ready For Development</option>
+                            <option value="Task In Progress">Task In Progress</option>
+                            <option value="Ready For Test">Ready For Test</option>
+                            <option value="Closed">Closed</option>
+                        </select>
+                        <button type='submit' className='task_submit'>+Add</button>
+                    </div>
+
                 </div>
-
-                <select className='task_status'>
-                    <option value="Ready For Development">Ready For Development</option>
-                    <option value="Task In Progress">Task In Progress</option>
-                    <option value="Ready For Test">Ready For Test</option>
-                    <option value="Closed">Closed</option>
-                </select>
-
-                <button type='submit' className='task_submit'>+Add</button>
             </form>
 
         </header>
