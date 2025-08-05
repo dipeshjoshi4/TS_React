82598eab4541eff3ad891e57203503910ab72d51
Your Name <dipeshj2310@gmail.com>
2025-08-05 16:01:31 +0530

    module-4 updated

--- Module-4/jiraboard/src/components/TaskColumn/task-column.jsx
+++ Module-4/jiraboard/src/components/TaskColumn/task-column.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import "./task-column.css"
+import TaskCard from '../TaskCard/TaskCard';
 
 const TaskColumn = (props) => {
     return (
@@ -7,7 +8,9 @@ const TaskColumn = (props) => {
             <section className="task_Column">
                 <h2 className='task_column_heading'>
                     <img src={props.icon} alt='' className='task_column_icon'></img>
-                    {props.title}</h2>
+                    {props.title}
+                </h2>
+                <TaskCard />
             </section>
         </div>
     )
