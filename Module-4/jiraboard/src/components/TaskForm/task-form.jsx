4e38c8df3063626f56c74e070988894ee4b4ce86
Your Name <dipeshj2310@gmail.com>
2025-08-07 10:53:38 +0530

    module-5 lecture-1 added

--- Module-4/jiraboard/src/components/TaskForm/task-form.jsx
+++ Module-4/jiraboard/src/components/TaskForm/task-form.jsx
@@ -1,13 +1,25 @@
 import "./task-form.css";
 import Tag from "../Tag/Tag";
+import { useState } from "react";
 
 const TaskForm = () => {
+
+    const [task, setTask] = useState("");
+    const handleTask = (e) => {
+        setTask(e.target.value)
+    };
+    console.log(task)
+
+    const [status, setStatus] = useState("");
+    const handleStatus = (e) => {
+        setStatus(e.target.value)
+    };
+    console.log(status)
+
     return (
         <header className="app_header">
             <form>
-                
-                <input type='text' placeholder='Enter Class Details' className='task_input' />
-
+                <input type='text' placeholder='Enter Class Details' className='task_input' onChange={handleTask} />
                 <div className='task_form_bottom'>
                     <div>
                         <Tag tagName="DEV"/>
@@ -15,7 +27,7 @@ const TaskForm = () => {
                         <Tag tagName="Product Owner" />
                     </div>
                     <div>
-                        <select className='task_status'>
+                        <select className='task_status' onChange={handleStatus}>
                             <option value="Ready For Development">Ready For Development</option>
                             <option value="Task In Progress">Task In Progress</option>
                             <option value="Ready For Test">Ready For Test</option>
@@ -24,7 +36,6 @@ const TaskForm = () => {
                         <button type='submit' className='task_submit'>+Add</button>
                     </div>
                 </div>
-
             </form>
 
         </header>
--- Module-5/Note.js
+++ Module-5/Note.js
@@ -1,7 +1,18 @@
 
-//?Module-5 
+//! Module-5
 
-//!-Notes only code will be still in Module -4 Folder structure
-//!-In module-5 we will go for the logic part how dynamic we change the data
+//!-In module-5 we will go for the logic part how dynamic we change the data Notes only code will be still in Module -4 Folder structure
 
-//?last
+//?66 lecture-01-Integrating Functionality: Understanding Requirements and Application Behavior.
+
+
+//?67 Lecture-1-Handling Form Submissions Efficiently
+//- talks about all the requirement menas drop and and drags the functionality ,delete CRUD
+//- We just make a our different functionality in in array of object and make Map function and iterate and given all info. while user interact
+//- this is in short wee will do in this whole Module
+
+
+
+
+
+//?68 Lecture-2-Pro Tip: Shortcut for Handling Complex Forms
