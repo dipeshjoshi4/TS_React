fb449b7e170abd49b2d34cd2d45b52d679c1d63b
Your Name <dipeshj2310@gmail.com>
2025-08-08 10:55:12 +0530

    add some fund

--- Module-4/Notes.js
+++ Module-4/Notes.js
@@ -233,4 +233,4 @@ export default TaskColumn;
 }
   */
 
-//?65 Lecture-8-
+
--- Module-5/Note.js
+++ Module-5/Note.js
@@ -11,8 +11,47 @@
 //- We just make a our different functionality in in array of object and make Map function and iterate and given all info. while user interact
 //- this is in short wee will do in this whole Module
 
+//?task-form.jsx
+/*
 
+import "./task-form.css";
+import Tag from "../Tag/Tag";
+import { useState } from "react";
 
+const TaskForm = () => {
+    const [task, setTask] = useState("");
+    const handleTask = (e) => {
+        setTask(e.target.value)
+    };
+    console.log(task)
+    const [status, setStatus] = useState("");
+    const handleStatus = (e) => {
+        setStatus(e.target.value)
+    };
+    console.log(status)
+    return (
+        <header className="app_header">
+                <input type='text' placeholder='Enter Class Details' className='task_input' onChange={handleTask} />
+                        <select className='task_status' onChange={handleStatus}>
+                            <option value="Ready For Development">Ready For Development</option>
+                            <option value="Task In Progress">Task In Progress</option>
+                            <option value="Ready For Test">Ready For Test</option>
+                            <option value="Closed">Closed</option>
+                        </select>
+        </header>
+    )
+}
+export default TaskForm
 
+*/
+
+// - here for the input filed we have different state and for the status chnage we have different state
+// - we can optimize this thing
 
 //?68 Lecture-2-Pro Tip: Shortcut for Handling Complex Forms
+
+//-there were many times we have complex forms. to handle we can messure many things
+
+//-first of all chnage we can not do the same state then for many complex forms have 5-6 chnages and thast we have to maintain 5-6 state completely injustice to react first rule -- reuse
+
+//-
