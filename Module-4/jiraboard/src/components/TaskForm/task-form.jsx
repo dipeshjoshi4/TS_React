58d7f1e1af56917027324f78acd94066aec18789
Your Name <dipeshj2310@gmail.com>
2025-08-09 14:55:32 +0530

    add status and task

--- Module-4/jiraboard/src/components/TaskForm/task-form.jsx
+++ Module-4/jiraboard/src/components/TaskForm/task-form.jsx
@@ -4,22 +4,39 @@ import { useState } from "react";
 
 const TaskForm = () => {
 
-    const [task, setTask] = useState("");
-    const handleTask = (e) => {
-        setTask(e.target.value)
-    };
-    console.log(task)
+    const [taskData, setTaskData] = useState({
+        task: "",
+        status: "Ready For Development",
+    })
 
-    const [status, setStatus] = useState("");
-    const handleStatus = (e) => {
-        setStatus(e.target.value)
-    };
-    console.log(status)
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setTaskData((prev) => {
+            return { ...prev, [name]: value }
+        })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(taskData);
+    }
+
+
+    // const [task, setTask] = useState("");
+    // const handleTask = (e) => {
+    //     setTask(e.target.value)
+    // };
+    // console.log(task)
+    // const [status, setStatus] = useState("");
+    // const handleStatus = (e) => {
+    //     setStatus(e.target.value)
+    // };
+    // console.log(status)
 
     return (
         <header className="app_header">
-            <form>
-                <input type='text' placeholder='Enter Class Details' className='task_input' onChange={handleTask} />
+            <form onSubmit={handleSubmit}>
+                <input type='text' placeholder='Enter Class Details' className='task_input' name="task" onChange={handleChange} />
                 <div className='task_form_bottom'>
                     <div>
                         <Tag tagName="DEV"/>
@@ -27,7 +44,7 @@ const TaskForm = () => {
                         <Tag tagName="Product Owner" />
                     </div>
                     <div>
-                        <select className='task_status' onChange={handleStatus}>
+                        <select className='task_status' onChange={handleChange} name="status">
                             <option value="Ready For Development">Ready For Development</option>
                             <option value="Task In Progress">Task In Progress</option>
                             <option value="Ready For Test">Ready For Test</option>
