4090a1dada0cca3d1c77cd196029d12582a2f77e
Your Name <dipeshj2310@gmail.com>
2025-08-16 18:02:33 +0530

    start Module 6 and add module6

---
+++ Module-6/Notes6.js
@@ -0,0 +1,14 @@
+
+//!-------------------------------------------Module-6-React Router--------------------------------------
+
+//?85 Lecture-1-Introduction to React Routing : Setting Up Your Project
+
+
+/*
+* Lecture Summary
+
+- So, in this lecture, I'm going to explain the importance of React Routing—why it's a crucial feature in React and how it can really benefit you.Essentially, we’ll look at how you can integrate React Routing into an existing codebase. I've put together a bit of a boilerplate code to help you understand how to approach an existing codebase and then add React Routing on top of it. We’ve also done a bit of refinement and added some CSS just to give you a feel for how it all fits together. And that’s it for this lecture. In the next one, we'll dive deeper into more concepts around React Routing.
+
+
+*/
+
