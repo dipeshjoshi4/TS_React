c66558b77fc3f4954e8f4bd93e301c0f47e93acd
Your Name <dipeshj2310@gmail.com>
2025-08-16 18:17:31 +0530

    making different component for routing

---
+++ Module-6/react-router-template/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import React from "react";
+
+function App() {
+  return (
+    <div className='logo'>
+      <main className='app-main'></main>
+    </div>
+  )
+}
+
+export default App
