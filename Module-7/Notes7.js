21f64c5e95f9e2a1ae3bb3683ba8214227b90535
dipeshjoshi4 <dipeshj2310@gmail.com>
2025-08-25 18:08:09 +0530

    add some intro on how to work with backend

---
+++ Module-7/Notes7.js
@@ -0,0 +1,16 @@
+
+//!---------------------------------------Module 7: API Integration & Error Handling in React
+// Module-7-
+
+
+//?97 Lecture-1-Frontend & Backend Collaboration: API & Error Handling
+/*
+
+- In this lecture, I’ve discussed how a frontend developer collaborates with the backend team.
+- We talked about the roles and responsibilities on both sides—what the frontend handles and what the backend team is responsible for.
+- Since this module is all about API integration and error handling, naturally we’re covering how frontend and backend developers work together. I’ve tried to give you a sense of how this collaboration looks in a company and what we’re going to cover in this module.
+
+
+
+
+*/
