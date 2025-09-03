c44637b110d803b8adb6090445f04782f5eb8fb9
dipeshjoshi4 <dipeshj2310@gmail.com>
2025-09-03 19:01:30 +0530

    add module-8 lecture-110

---
+++ Module-8/Notes8.js
@@ -0,0 +1,14 @@
+//!-----------------------------Module 8: React Design Patterns----------------------
+
+//1-Conatiner Presentation Design Pattern
+//2-Custom Hook Pattern -Imp
+//3-High Order Component Pattern
+
+// - these three design patterns are very commonly seen in React Projects,you should Definetly
+//- For code readable,scalbility
+
+
+//?110 Lecture-1-Mastering React Design Patterns: Introduction
+
+// Purpose is just maintain and scalable 
+// Bisiness and Design part on some structure
---
+++ Module-8/react-design-patterns/src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+
+
+  return (
+    <>
+      <h1>Conatiner Presentation Design Pattern</h1>
+    </>
+  )
+}
+
+export default App
