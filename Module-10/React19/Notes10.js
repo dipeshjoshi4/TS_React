7deb2f9c529ca2374efa2d663bb8c845983313bd
dipeshjoshi4 <dipeshj2310@gmail.com>
2025-09-20 15:01:41 +0530

    add module-10

---
+++ Module-10/React19/Notes10.js
@@ -0,0 +1,8 @@
+//!133-lecture-1-React 19 Introduction: useActionState() – Modern Form & State Handling
+
+/*
+- In this lecture, we learned about the new useActionState hook introduced in React 19. 
+- This hook makes form handling much easier and more efficient. Instead of creating separate states for data, errors, and loading, useActionState lets you manage everything in a simpler way.
+- It reduces the amount of code, keeps it more readable, and helps in handling complex forms effectively. 
+- React 19 introduces this hook as part of its focus on improving form management and working with complex data.
+*/
