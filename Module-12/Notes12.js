4eedf49d002259091f932f78f4be8a494e89a2b3
dipeshjoshi4 <dipeshj2310@gmail.com>
2025-10-07 20:29:01 +0530

    lecture-161 added

--- Module-12/Notes12.js
+++ Module-12/Notes12.js
@@ -1,6 +1,8 @@
 
 //!---------------------Module 12: Testing React applications Using Jest------------------
 
+const { totalmem } = require("os")
+
 //!160-Lec-1-React Testing Simplified: Mastering Jest and Component-Level Testing
 
 //?Notes
@@ -20,12 +22,11 @@
 //Focuses on testing behaviour from a user's perspective rather than implementation details.
 //together with Jest as the test runner.
 
-//?3 Types
+//?4 Types/ Many
 //1.Unit Testing(Here) -Test Coverage |Branch Coverage |Code Deploy after certain limit above your code can deploy
 //2.Intergation Testing
 //3.System Testing
 //4.Performance Testing
-
 
 //?setUp Steps
 
@@ -39,4 +40,61 @@
 // "test": "echo \"Error: no test specified\" && exit 1" =>chnage into
 // "test": "jest"
 
+//=> when we write => npm test
+//=> no test found because no test file
+
+//!161-lec-2-Getting Started with Jest: How Jest Runner Finds and Executes Your Tests
+
+/*
+In this lecture, we explored what the Jest runner is and how it automatically detects your test files in your project.
+We covered how to create test files, write simple test cases, and run them seamlessly using Jest.
+By the end of this lecture, you’ll have a solid understanding of how to get started with Jest and why it’s a great tool for running your tests effortlessly.
+*/
+
 //?Why jest is test runner only ?
+
+//?How to Creat test file and run what the answer of run
+
+//- test file should name as        //? filname.test.js
+//- if empty test file we run then  //? error:it should be one test case
+//- syntax                          //? test("My first test file", () => { })
+
+//?answer
+// test suits:  1 passed 1 total
+// Tests: 1passed, 1 total
+// Snapshots: 0
+// Time:2.322s
+// Ran all test suites
+
+//?You can write test file as
+//first.test.js
+//second.spec.js
+
+//?folder structure
+// - __test__ => all test file in this naming converstional folder structure
+
+/*
+if User is Component
+User ->
+1st file => User.js
+1st Folder => __test__
+under that folder you have that comp test file => User.test.js
+*/
+
+//?There are many testing file
+
+// cypresss
+// mocha
+// enzyme
+
+//!162-lec-3-Assertions in Jest: Writing Smart and Clear Test Cases
+
+/*
+In This Lecture we dived into the concept of assertions—understanding
+how to compare the expected and received values to ensure our code behaves as intended.
+We discussed how to write assertions effectively and how to structure your test suites and test cases for maximum clarity.
+By the end of this session, you'll have a strong grasp of how to use Jest assertions to write smart and reliable test cases.
+*/
+
+// we have 3 test suits
+// every suits have mutiple test  can happen
