56dd98b5d8809fa4bb06ab184c92c67f59208643
dipeshjoshi4 <dipeshj2310@gmail.com>
2025-11-19 23:16:09 +0530

    add the module-14 question-11

---
+++ Module-14/15/search/src/App.jsx
@@ -0,0 +1,12 @@
+import SearchBar from "./Components/SearchBar"
+
+function App() {
+  return (
+    <>
+      <h5>Search Bar Input Show the Item List . Means Input is One Kind of Query</h5>
+      <SearchBar />
+    </>
+  )
+}
+
+export default App
